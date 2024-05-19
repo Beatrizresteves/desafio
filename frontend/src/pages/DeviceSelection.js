920f21a0e77869ae8ede7af2845073a4daafcd44
Beatrizresteves <Beatrizresteves@github.com>
2024-05-19 14:42:12 -0300

    Add devices

---
+++ frontend/src/pages/DeviceSelection.js
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const DeviceSelection = () => {
+  const [devices, setDevices] = useState([]);
+  const [selectedDevices, setSelectedDevices] = useState([]);
+
+  useEffect(() => {
+    const fetchDevices = async () => {
+      try {
+        const res = await axios.get('/api/devices');
+        setDevices(res.data);
+      } catch (err) {
+        console.error('Error fetching devices', err);
+      }
+    };
+    fetchDevices();
+  }, []);
+
+  const handleSelect = (device) => {
+    setSelectedDevices([...selectedDevices, device]);
+  };
+
+  return (
+    <div>
+      <h1>Select Devices</h1>
+      <ul>
+        {devices.map((device) => (
+          <li key={device._id}>
+            {device.name} <button onClick={() => handleSelect(device)}>Select</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default DeviceSelection;
